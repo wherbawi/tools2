775595053afe972aee94c9c93bc4ff5632dde034
Wesam Herbawi <wesam.herbawi@gmail.com>
2018-04-07 21:26:32 +0200

    added yaml file

---
+++ src/main/java/edu/ppu/runners/PolygonArea.java
@@ -0,0 +1,15 @@
+package edu.ppu.runners;
+
+import org.geotools.referencing.GeodeticCalculator;
+
+public class PolygonArea {
+
+	public static void main(String[] args) {
+		GeodeticCalculator c = new GeodeticCalculator();
+		c.setStartingGeographicPoint(48.764148, 9.168008);
+
+		c.setDestinationGeographicPoint(48.690815, 9.194553);
+		System.out.println(c.getOrthodromicDistance());
+	}
+
+}
